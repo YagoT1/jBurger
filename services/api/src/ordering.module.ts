ec48b857027444c916cd557e8f4d3c5927afe2aa
Yago Torres <yagotorres075@gmail.com>
2026-07-20 12:28:45 -0300

    chore: remove legacy commerce/ordering layer after rebase

--- services/api/src/ordering.module.ts
+++
@@ -1,4 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CartController, OrderDraftsController, OrdersController } from './ordering.controller.js';
-@Module({ controllers: [CartController, OrderDraftsController, OrdersController] })
-export class OrderingModule {}
